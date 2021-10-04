959f91e28fc070c795ceddc412a68787d2dd3ef7
Shaps Benkau <shapsuk@me.com>
2021-10-04 13:37:00 +0100

    Adds tvOS support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "Containers",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         .library(
